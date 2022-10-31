9dccebe854a2183104705f3a748be3ee48aed293
Stéphane Francel <stephane.francel@gmail.com>
2022-10-31 01:21:57 +0100

    App scaffolding

---
+++ src/app/shared/components/header/header.component.spec.ts
@@ -0,0 +1,27 @@
+import { APP_NAME } from 'src/app/app.provider';
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HeaderComponent } from './header.component';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+      providers: [{ provide: APP_NAME, useValue: 'app name' }],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
