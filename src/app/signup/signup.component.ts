717028e276ae130484305e50d3ea6e3baf7941d8
Stéphane Francel <stephane.francel@gmail.com>
2022-11-01 14:34:22 +0100

    Update to Angular v14

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { AbstractControl, UntypedFormBuilder, UntypedFormControl, Validators } from '@angular/forms';
 
 import { AuthService } from '../shared/services/auth.service';
 import { SignupForm } from './signup.types';
@@ -15,11 +15,11 @@ export class SignupComponent {
     eula: [false, [Validators.requiredTrue]],
   });
 
-  email = this.signup.get('email') as FormControl;
+  email = this.signup.get('email') as UntypedFormControl;
 
-  password = this.signup.get('password') as FormControl;
+  password = this.signup.get('password') as UntypedFormControl;
 
-  eula = this.signup.get('eula') as FormControl;
+  eula = this.signup.get('eula') as UntypedFormControl;
 
   getValidationClasses(control: AbstractControl) {
     return {
@@ -28,7 +28,7 @@ export class SignupComponent {
     };
   }
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService) {}
+  constructor(private formBuilder: UntypedFormBuilder, private authService: AuthService) {}
 
   resetEmail() {
     this.signup.controls['email'].reset();
